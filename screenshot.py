8dfff7b1f490746b2db41e2a79d818ef9869cf94
ConquerorFY <ryanrlfy33@gmail.com>
2024-02-15 17:24:21 +0800

    Remove Excess Files

--- screenshot.py
+++
@@ -1,9 +0,0 @@
-from PIL import ImageGrab
-import win32gui
-
-hwnd = win32gui.FindWindow(None, r'Task Manager')
-win32gui.SetForegroundWindow(hwnd)
-dimensions = win32gui.GetWindowRect(hwnd)
-
-image = ImageGrab.grab(dimensions)
-image.show()
